004b4f06f955b9f0b0e7ea3e461248e7b0dad874
nenoleni <117370087+nenoleni@users.noreply.github.com>
2022-11-03 21:03:47 +0700

    Create crypt.c

---
+++ crypt.c
@@ -0,0 +1,2 @@
+ <script nonce="JiimY3gWzMosk6lao5tILOxG2BLlbgeyM/ToRqTsI/k="> window.NONCE_ID="JiimY3gWzMosk6lao5tILOxG2BLlbgeyM/ToRqTsI/k=";</script>
+        <script nonce="JiimY3gWzMosk6lao5tILOxG2BLlbgeyM/ToRqTsI/k=">
